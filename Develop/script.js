5b2d8b1f9ad2a0f6c13205cc56915528a8feef2f
jerry <jerry4905@hotmail.com>
2021-05-09 09:23:33 -0700

    password generated

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,8 @@ function generatePassword(){
   var passwordLength = prompt("lenght of password between 8 and 128?");
   passwordLength = parseInt(passwordLength);
    console.log(passwordLength);
-
+   
+   // confirm passwrod length is between 8 and 128
    if (passwordLength > 128 || passwordLength < 8){
     alert("select a length between 8 and 128");
     generatePassword();
@@ -24,13 +25,24 @@ function generatePassword(){
 
   var passwordString = "";
 
+  // run test to create password with selected options
   while (passwordString.length <passwordLength){
 
     if (passwordLowerCase){passwordString += lowerCase[Math.floor(Math.random() * lowerCase.length)]}
     if (passwordUpperCase){passwordString += upperCase[Math.floor(Math.random() * upperCase.length)]}
-    if (passwordNumeric){passwordString += nuberList[Math.floor(Math.random() * numberList.length)]}
+    if (passwordNumeric){passwordString += numberList[Math.floor(Math.random() * numberList.length)]}
     if (passwordSpecial){passwordString += specialCharacters[Math.floor(Math.random() * specialCharacters.length)]}
     
+    // confirm atlest one option was selected for passowrd
+    if (
+      (passwordLowerCase === false && 
+      passwordUpperCase === false && 
+      passwordNumeric === false && 
+      passwordSpecial === false )){
+      alert("atleast one option should be selected");
+      generatePassword();
+
+    };
   }
 
   return passwordString
